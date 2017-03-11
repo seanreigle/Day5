73d310ca1f3c1416e7f7253f968be847525f2b34
Sean Reigle <sereigle@gmail.com>
2017-03-10 22:26:45 -0800

    Problem 4

--- app.js
+++ app.js
@@ -70,7 +70,7 @@ function sumArray(testArray){ //eslint-disable-line
   var firstSum = (testArray[0], testArray[2]);
   var secondSum = testArray[0] + ',' + testArray[1] + ',' + testArray[2] + ' was passed in as an array of numbers, and ' + secondSum + 'is their sum.';
 }
-sumArray(testArray);
+sumArray();
 // Here is the test for sumArray(); uncomment it to run it
 
 testSumArray(testArray);
